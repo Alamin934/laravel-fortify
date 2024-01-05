6b77e8177bcc72a2b52a38745243ce5d44e30534
Alamin934 <islamalamin934@gmail.com>
2024-01-05 17:22:50 +0600

    Profile information update system added

---
+++ resources/views/auth/update-profile.blade.php
@@ -0,0 +1,36 @@
+@extends('auth.layouts.auth-layout')
+@section('auth-content')
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="w-50 mx-auto">
+                <h2>Update Profile</h2>
+                <form action="{{ route('user-profile-information.update') }}" method="POST" class="mt-3">
+                    @csrf
+                    @method('put')
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                            id="name" value="{{old('name')}}">
+                        @error('name')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            id="email" value="{{old('email')}}">
+                        @error('email')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </form>
+                @if (session('status'))
+                <div class="alert alert-success">User Information has been updated</div>
+                @endif
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
